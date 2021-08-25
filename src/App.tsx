ef94aa4f00a3fda4757c2ece403db5a72fc288c6
Anh.Tuan <nttanh6299@gmail.com>
2021-08-25 20:35:29 +0700

    Use styled-components instead

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,5 @@
-import { Button, Pane, Text, majorScale } from 'evergreen-ui'
-
 function App() {
-  return (
-    <Pane display="flex" alignItems="center" marginX={majorScale(2)}>
-      <Button>Learn React</Button>
-      <Text>This is a clickable Button</Text>
-    </Pane>
-  )
+  return <div>Learn React</div>
 }
 
 export default App
