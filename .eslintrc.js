4d953dea49ed88341ba9e1a6890b4c27b79e3b0c
Anh.Tuan <nttanh6299@gmail.com>
2021-09-26 21:58:08 +0700

    Update workspace

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'prettier',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/prop-types': 0,
+    'linebreak-style': ['error', 'unix'],
+    'arrow-body-style': 'off',
+    'prefer-arrow-callback': 'off',
+  },
+}
