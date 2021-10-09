40645ef06645fbb4049cde9be99c63d4ef72468a
Anh.Tuan <nttanh6299@gmail.com>
2021-10-09 23:45:54 +0700

    Add Bg color to container

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -1,9 +1,11 @@
 import styled from 'styled-components'
 
-const Container = styled.div`
+const Container = styled.div<{ bgColor?: string }>`
   width: 100%;
   max-width: 1200px;
   margin: 0 auto;
+
+  background-color: ${(props) => props.bgColor ?? 'transparent'};
 `
 
 export default Container
